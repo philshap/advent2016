5f2a1c743ca41f6aa3fc095087d37bbffe136f7e
philshap <philshap@gmail.com>
2023-11-23 23:25:31 -0500

    day 12

--- src/Main.java
+++ src/Main.java
@@ -30,7 +30,7 @@ public class Main implements Support {
 
     private void runDays() throws Exception {
         Day[] days = {new Day1(), new Day2(), new Day3(), new Day4(), new Day5(), new Day6(),
-                new Day7(), new Day8(), new Day9(), new Day10()};
+                new Day7(), new Day8(), new Day9(), new Day10(), new Day12()};
         for (var day : days) {
             day.run(this);
         }
