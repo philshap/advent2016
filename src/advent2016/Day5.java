5e19473b3b119470ec9250fdcc5c432d16746f35
philshap <philshap@gmail.com>
2023-11-27 08:44:34 -0500

    remove unnecessary string conversion in md5 code

--- src/advent2016/Day5.java
+++ src/advent2016/Day5.java
@@ -1,6 +1,5 @@
 package advent2016;
 
-import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -24,44 +23,46 @@ public class Day5 implements Day {
     @Override
     public void run(Support support) throws Exception {
         var input = support.readString(5);
-        if (support.includeSlow()) {
-            part1(input);
-            part2(input);
-        } else {
-            System.out.println("day 5 part 1: too slow, skipping");
-            System.out.println("day 5 part 2: too slow, skipping");
-        }
+        part1(input);
+        part2(input);
     }
 
-    String computeHash(String prefix, int count) {
+    byte[] computeHash(String prefix, int count) {
         md5.reset();
         md5.update(StandardCharsets.UTF_8.encode(prefix + count));
-        return String.format("%032x", new BigInteger(1, md5.digest()));
+        return md5.digest();
     }
+
+    // Bytes in the MD5 digest are split into two characters in an MD5 hex string.
+    // To check for five leading zeros, we check the first five 4-bit values in the byte array.
+    private boolean startsWithZeros(byte[] hash) {
+        return hash[0] == 0 && hash[1] == 0 && (hash[2] >> 4) == 0;
+    }
+
+    static final Map<Integer, Character> INT_TO_CHAR =
+            IntStream.range(0, 16).boxed()
+                    .collect(Collectors.toMap(Function.identity(), i -> Integer.toHexString(i).charAt(0)));
 
     private void part1(String prefix) {
         var password = IntStream.iterate(0, i -> i + 1)
                 .mapToObj(i -> computeHash(prefix, i))
-                .filter(s -> s.startsWith("00000"))
+                .filter(this::startsWithZeros)
+                .map(hash -> INT_TO_CHAR.get(hash[2] & 0xf))
                 .limit(8)
-                .map(s -> s.charAt(5))
                 .collect(Support.collectToString());
         System.out.printf("day 5 part 1: %s%n", password);
     }
-
-    static final Map<Character, Integer> CHAR_TO_INT =
-            IntStream.range(0, 8).boxed().collect(Collectors.toMap(i -> (char) ('0' + i), Function.identity()));
 
     private void part2(String prefix) {
         char[] password = new char[8];
         Arrays.fill(password, '.');
         var foundPass = IntStream.iterate(0, i -> i + 1)
                 .mapToObj(i -> computeHash(prefix, i))
-                .filter(s -> s.startsWith("00000"))
+                .filter(this::startsWithZeros)
                 .map(hash -> {
-                    Integer pos = CHAR_TO_INT.get(hash.charAt(5));
-                    if (pos != null && password[pos] == '.') {
-                        password[pos] = hash.charAt(6);
+                    int pos = hash[2] & 0xf;
+                    if (pos < password.length && password[pos] == '.') {
+                        password[pos] = INT_TO_CHAR.get((hash[3] >> 4) & 0xf);
                     }
                     return new String(password);
                 }).dropWhile(s -> s.contains("."))
