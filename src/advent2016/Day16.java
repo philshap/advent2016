063643c24785aea443af2e95747346fc6c2ab616
Phil Shapiro <philshap@gmail.com>
2025-10-25 15:11:24 -0400

    day 16 part 2

--- src/advent2016/Day16.java
+++ src/advent2016/Day16.java
@@ -7,13 +7,14 @@ public class Day16 extends Day {
     super(16);
   }
 
-  static final int LENGTH = 272;
-
   static class Data {
-    final int[] data = new int[LENGTH * 2 + 1];
+    final int length;
+    final int[] data;
     int size;
 
-    Data(String initial) {
+    Data(String initial, int length) {
+      this.length = length;
+      data = new int[length * 2 + 1];
       int i = 0;
       for (byte b : initial.getBytes()) {
         data[i++] = b;
@@ -40,25 +41,36 @@ public class Day16 extends Day {
       System.arraycopy(checksum, 0, data, 0, j);
     }
 
-    String print() {
-      String s = Arrays.stream(data).mapToObj(i -> (char) i).collect(Support.collectToString());
-      return s.substring(0, size);
+    String computeChecksum() {
+      while (size < length) {
+        expand();
+      }
+      size = length;
+      do {
+        checksum();
+      } while (size % 2 == 0);
+
+      return Arrays.stream(data).limit(size).mapToObj(i -> (char) i).collect(Support.collectToString());
     }
   }
 
   String part1() {
-    Data d = new Data(data);
-    while (d.size < LENGTH) {
-      d.expand();
-    }
-    d.size = LENGTH;
-    do {
-      d.checksum();
-    } while (d.size % 2 == 0);
-    return String.valueOf(d.print());
+    Data d = new Data(data, 272);
+    return d.computeChecksum();
   }
 
   String part2() {
-    return "???";
+    Data d = new Data(data, 35651584);
+    return d.computeChecksum();
+  }
+
+  public static void main(String[] args) {
+    Day day = new Day16() {
+      @Override
+      String getData() {
+        return "11110010111001001";
+      }
+    };
+    day.run("01110011101111011", "11001111011000111");
   }
 }
